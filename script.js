2c311d62190770a4111d3d1592e94747acdd723c
Gerald Andrews <gerald2399@comcast.net>
2022-03-26 16:01:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ window.onclick = function(e) {
 
 // AOS Animation
 AOS.init({
-  // once: true,
+   once: true,
   // offset: 200, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 1000 // values from 0 to 3000, with step 50ms
