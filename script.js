00b9a00d431c906860b8070245a23b3c224b9568
Gerald Andrews <gerald2399@comcast.net>
2022-04-26 22:49:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,6 @@ AOS.init({
    once: true,
   // offset: 200, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000 // values from 0 to 3000, with step 50ms
+  duration: 1500 // values from 0 to 3000, with step 50ms
 });
 
